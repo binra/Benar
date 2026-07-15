a07c8d90b9371d170c4e476b43127972c2aa141e
binra <79102117+binra@users.noreply.github.com>
2026-07-15 23:43:06 +0300

    update script.js

--- script.js
+++ script.js
@@ -18,16 +18,10 @@ async function loadProducts() {
 
         const data = product.data();
 
-        // Featured Products
-        if (data.featured) return;
-
-        // Best Deals
-        if (data.bestDeal) return;
-
-        // New Arrivals
-        if (data.newArrival) return;
-
-        const container = document.getElementById("products");
+        const container =
+            data.featured
+                ? document.getElementById("featuredProducts")
+                : document.getElementById("products");
 
         if (!container) return;
 
