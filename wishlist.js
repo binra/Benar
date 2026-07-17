e9bff5413a989740c5e54e1d7542614b20dce576
binra <79102117+binra@users.noreply.github.com>
2026-07-17 16:24:30 +0300

    git add. add commit -m " add wishlist.html, wishlist.js" git push

---
+++ wishlist.js
@@ -0,0 +1,57 @@
+import { db } from "./firebase.js";
+
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const container = document.getElementById("wishlistProducts");
+
+async function loadWishlist() {
+
+    const wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+
+    container.innerHTML = "";
+
+    if (wishlist.length === 0) {
+
+        container.innerHTML = `
+            <h2 style="text-align:center;width:100%;">
+                ❤️ Your wishlist is empty
+            </h2>
+        `;
+
+        return;
+    }
+
+    const snapshot = await getDocs(collection(db, "products"));
+
+    snapshot.forEach((doc) => {
+
+        if (!wishlist.includes(doc.id)) return;
+
+        const data = doc.data();
+
+        container.innerHTML += `
+        <div class="product">
+
+            <img src="${data.image}" alt="${data.title}">
+
+            <h2>${data.title}</h2>
+
+            <p class="price">$${data.price}</p>
+
+            <a href="product.html?id=${doc.id}" class="buy-btn">
+
+                View Product
+
+            </a>
+
+        </div>
+        `;
+
+    });
+
+}
+
+loadWishlist();
